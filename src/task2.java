77276d6d9502766b8ecd6d8a7446279b503f1133
ksuhavr <petrovich_kseniya@mail.ru>
2025-05-08 12:02:17 +0300

    test commit

---
+++ src/task2.java
@@ -0,0 +1,14 @@
+public class task2 {
+    public static void checkSumSign() {
+        int a = 1, b = 2;
+        if (a+b >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void main (String[] args) {
+        checkSumSign();
+    }
+}
---
+++ src/task3.java
@@ -0,0 +1,17 @@
+public class task3 {
+    public static void printColor() {
+        int value = 10;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        else if (value >0 && value <=100) {
+            System.out.println("Желтый");
+        }
+        else if (value >100) {
+            System.out.println("Зеленый");
+        }
+    }
+    public static void main(String[] args) {
+        printColor();
+    }
+}
